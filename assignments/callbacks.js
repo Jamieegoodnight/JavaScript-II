3be37403ed9b01fdc9c3aedcccba63e1469514d1
Jamie Goodnight <goodnightlambda@gmail.com>
2019-01-22 15:10:01 -0500

    Callback challenges? More like callback EASIES!

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -22,31 +22,92 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 */
 
 
-function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+// function getLength(arr, cb) {
+//   // getLength passes the length of the array into the callback.
+// }
+
+function getLength(arr, cb){
+  return cb(arr.length);
 }
 
-function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+getLength(items, function(x){
+  console.log(x);
+})
+
+
+
+// function last(arr, cb) {
+//   // last passes the last item of the array into the callback.
+
+// }
+
+function last(arr, cb){
+  console.log(arr[arr.length-1]);
 }
 
-function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+last(items, function(x){
+ console.log(x);
+})
+
+// function sumNums(x, y, cb) {
+//   // sumNums adds two numbers (x, y) and passes the result to the callback.
+// }
+
+function sumNums(x,y,cb){
+ return cb(x+y);
 }
 
-function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+sumNums(2,2, function(x){
+  console.log(x);
+})
+
+// function multiplyNums(x, y, cb) {
+//   // multiplyNums multiplies two numbers and passes the result to the callback.
+// }
+
+function multiplyNums(x, y, cb){
+  return cb(x*y);
 }
 
-function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+multiplyNums(2,2,function(x){
+  console.log(x);
+})
+
+// function contains(item, list, cb) {
+//   // contains checks if an item is present inside of the given array/list.
+//   // Pass true to the callback if it is, otherwise pass false.
+// }
+
+function contains(item, list, cb){
+  for (let i=0; i<list.length; i++){
+    if (list[i]===item){
+      return cb(true);
+    } else {
+      return cb(false);
+    }
+  }
 }
 
-/* STRETCH PROBLEM */
+contains('Pencil', items, function(x){
+  console.log(x);
+})
 
-function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
-}
+
+
+
+
+
+
+
+
+
+
+
+
+// /* STRETCH PROBLEM */
+
+// function removeDuplicates(array, cb) {
+//   // removeDuplicates removes all duplicate values from the given array.
+//   // Pass the duplicate free array to the callback function.
+//   // Do not mutate the original array.
+// }
